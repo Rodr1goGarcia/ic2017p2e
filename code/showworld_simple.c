d486bbc9930d83b3b87f68d06515ff72ddcf8ff8
Rodr1goGarcia <gemeosgarcia@gmail.com>
2018-02-17 20:38:46 +0000

    ShowWorld

--- code/showworld_simple.c
+++ code/showworld_simple.c
@@ -1,19 +1,19 @@
 /*
- * This file is part of "Projeto de Introdução à Computação 2ª época 2017/2018"
- * (PIC2e1718).
+ * This file is part of "2º Projeto de Introdução à Computação 2017/2018"
+ * (2oPIC1718).
  *
- * PIC2e1718 is free software: you can redistribute it and/or modify
+ * 2oPIC1718 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * PIC2e1718 is distributed in the hope that it will be useful,
+ * 2oPIC1718 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PIC2e1718. If not, see <http://www.gnu.org/licenses/>.
+ * along with 2oPIC1718. If not, see <http://www.gnu.org/licenses/>.
  * */
 
 /**
@@ -24,7 +24,7 @@
  * @note Students should change or replace this file with a new implementation
  * making use of graphical or game library.
  *
- * @author Nuno Fachada
+ * @authors Nuno Fachada, André Rosa (a21704022), Ricardo Leal (a21703090), Rodrigo Garcia (a21704304)
  * @date 2018
  * @copyright [GNU General Public License version 3 (GPLv3)](http://www.gnu.org/licenses/gpl.html)
  * */
@@ -32,6 +32,18 @@
 #include "showworld.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <g2.h>
+#include <g2_X11.h>
+
+enum g2_colors {
+    WHITE = 0, /* Background */
+    BLACK = 1, /* Nothing */
+    BLUE= 3, /*Playable ZOmbie*/
+    GREEN = 7, /* Playable Agent */
+    GRAY = 14, /* Agent None */
+    RED = 19, /* Agent Zombies */
+    YELLOW = 25 /* Agent Humans */
+};
 
 /* The implementation of `SHOWWORLD` type used in this simple text-based world
  * visualization code. In this simple case, we only need to keep track of the
@@ -56,12 +68,13 @@ SHOWWORLD *showworld_new(
     unsigned int ydim,
     get_agent_info_at aginfo_func) {
 
-    SHOWWORLD *sw = NULL;
+	SHOWWORLD *sw = NULL;
     sw = malloc(sizeof(SHOWWORLD));
     sw->xdim = xdim;
     sw->ydim = ydim;
     sw->aginfo_func = aginfo_func;
-    return sw;
+    
+    return sw; 
 
 }
 
@@ -78,18 +91,56 @@ void showworld_destroy(SHOWWORLD *sw) {
  * This function obeys the `showworld_update()` prototype defined in
  * `showworld.h`. */
 void showworld_update(SHOWWORLD *sw, void *w) {
-
+	
+	int *device;
+    device = malloc(sizeof(int));
+	
+    /* Abrir janela */
+    *device = g2_open_X11(sw->xdim * 21.5, sw->ydim * 23.5);
+    
+    /* Desenhar o fundo */
+    g2_set_background(*device, WHITE);
+    
+    /* Especificar tamanho e forma dos agentes */
+    g2_set_QP(*device, 20, QPcirc);
+    
+    printf("\n");
+    
+    g2_pen(*device, GRAY);
+	g2_plot_QP(*device, 0.5, 0.5);
+    g2_pen(*device, GREEN);
+	g2_plot_QP(*device, 2.5, 0.5);
+	g2_pen(*device, BLACK);
+	g2_string(*device, sw->xdim * 2.75, sw->ydim*0.75, "HP");
+	g2_pen(*device, YELLOW);
+	g2_plot_QP(*device, 4.5, 0.5);
+	g2_pen(*device, BLACK);
+	g2_string(*device, sw->xdim * 4.75, sw->ydim*0.75, "H");
+	g2_pen(*device, BLUE);
+	g2_plot_QP(*device, 6.5, 0.5);
+	g2_pen(*device, BLACK);
+	g2_string(*device, sw->xdim * 6.75, sw->ydim*0.75, "ZP");
+	g2_pen(*device, RED);
+	g2_plot_QP(*device, 8.5, 0.5);
+	g2_pen(*device, BLACK);
+	g2_string(*device, sw->xdim * 8.75, sw->ydim*0.75, "Z");
+	g2_pen(*device, BLACK);
+	g2_plot_QP(*device, 10.5, 0.5);
+	g2_pen(*device, WHITE);
+	g2_string(*device, sw->xdim * 10.75, sw->ydim*0.75, "U");
+    
+        /* Print a newline after world is shown/updated. */
     printf("\n");
 
     /* Cycle through all the rows */
-    for (unsigned int y = 0; y < sw->ydim; ++y) {
+    for (unsigned int y = (sw->ydim); y > 0; --y) {
 
         /* Cycle through all the columns for the current row */
         for (unsigned int x = 0; x < sw->xdim; ++x) {
 
             /* Get state of the world (in bit packed fashion) using the user
                supplied function. */
-            unsigned int item = sw->aginfo_func(w, x, y);
+            unsigned int item = sw->aginfo_func(w, x, y-1);
 
             /* Extract the agent type (2 bits). */
             AGENT_TYPE ag_type = item & 0x3;
@@ -97,6 +148,7 @@ void showworld_update(SHOWWORLD *sw, void *w) {
             unsigned char playable = (item >> 2) & 0x1;
             /* Extract the agent ID (16 bits). */
             unsigned short ag_id = (item >> 3) & 0xFFFF;
+            
 
             /* Determine the agent type. */
             switch (ag_type) {
@@ -104,6 +156,8 @@ void showworld_update(SHOWWORLD *sw, void *w) {
                 /* If no agent is present at (x,y) just print a dot. */
                 case None:
                     printf(" .  ");
+                    g2_pen(*device, GRAY);
+                    g2_plot_QP(*device, x+0.5, y+1.5);
                     break;
 
                 /* If human agent present at (x,y) print 'h' or 'H'. */
@@ -111,9 +165,13 @@ void showworld_update(SHOWWORLD *sw, void *w) {
                     if (playable) {
                         /* Uppercase 'H' for player-controlled human agent. */
                         printf("H");
+                        g2_pen(*device, GREEN);
+                        g2_plot_QP(*device, x+0.5, y+1.5);
                     } else {
                         /* Lowercase 'h' for AI-controlled human agent. */
                         printf("h");
+                        g2_pen(*device, YELLOW);
+                        g2_plot_QP(*device, x+0.5, y+1.5);
                     }
                     /* Print the agent ID in front of the 'h'/'H'. */
                     printf("%02X ", ag_id);
@@ -124,9 +182,13 @@ void showworld_update(SHOWWORLD *sw, void *w) {
                     if (playable) {
                         /* Uppercase 'Z' for player-controlled zombie agent. */
                         printf("Z");
+                        g2_pen(*device, BLUE);
+                        g2_plot_QP(*device, x+0.5, y+1.5);
                     } else {
                         /* Lowercase 'z' for AI-controlled zombie agent. */
                         printf("z");
+                        g2_pen(*device, RED);
+                        g2_plot_QP(*device, x+0.5, y+1.5);
                     }
                     /* Print the agent ID in front of the 'h'/'H'. */
                     printf("%02X ", ag_id);
@@ -136,6 +198,8 @@ void showworld_update(SHOWWORLD *sw, void *w) {
                    happen. */
                 default:
                     printf("?   ");
+                    g2_pen(*device, BLACK);
+                    g2_plot_QP(*device, x+0.5, y+1.5);
 
             }
         }
@@ -144,8 +208,7 @@ void showworld_update(SHOWWORLD *sw, void *w) {
         printf("\n\n");
 
     }
-
+    
     /* Print a newline after world is shown/updated. */
     printf("\n");
-
 }
